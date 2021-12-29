57b3c30589da6215b247ed008099050934ce5bab
Milton <milton.dev.sis@gmail.com>
2021-12-29 11:51:49 -0300

    Create Todo database

---
+++ app/src/main/java/com/mpfcoding/app_mvvm_crud_todo_list/data/TodoDatabase.kt
@@ -0,0 +1,13 @@
+package com.mpfcoding.app_mvvm_crud_todo_list.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Todo::class],
+    version = 1
+)
+abstract class TodoDatabase: RoomDatabase() {
+
+    abstract val dao: TodoDao
+}
