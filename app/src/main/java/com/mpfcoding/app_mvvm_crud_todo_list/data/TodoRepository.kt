e0d34cd250dd3cb58aa67c3ef5bf382baab03236
Milton <milton.dev.sis@gmail.com>
2021-12-30 11:13:18 -0300

    Todo repository and implementation

---
+++ app/src/main/java/com/mpfcoding/app_mvvm_crud_todo_list/data/TodoRepository.kt
@@ -0,0 +1,14 @@
+package com.mpfcoding.app_mvvm_crud_todo_list.data
+
+import kotlinx.coroutines.flow.Flow
+
+interface TodoRepository {
+
+    suspend fun insertTodo(todo: Todo)
+
+    suspend fun deleteTodo(todo: Todo)
+
+    suspend fun getTodoById(id: Int): Todo?
+
+    fun getTodos(): Flow<List<Todo>>
+}
